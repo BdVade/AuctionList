c903597d20066872e436cc09f48ddc4a5a5ee920
Victor Aderibigbe <victoraderibigbe03@gmail.com>
2022-05-29 15:45:39 +0100

    Update models.py

--- core/models.py
+++ core/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class Auction(models.Model):
     name = models.CharField(max_length=40)
     top_bid = models.IntegerField()
-    top_bidder = models.CharField(max_length=200)
+    top_bidder = models.CharField(max_length=200, null=True)
     auction_info = models.CharField(max_length=200)
     floor_price = models.IntegerField()
